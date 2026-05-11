7e73c3b7a9c9995e951d7dfdf84b944f0f43b7b4
akmalropip <akmalropip@gmail.com>
2026-05-11 17:47:05 +0700

    menambahkan class admin

--- admin.h
+++ admin.h
@@ -3,3 +3,37 @@
 #include <vector>
 #include "user.h"
 #include "member.h"
+
+class admin : public user {
+public:
+    vector<member*> daftar_member;
+
+    admin(string pNama, string pEmail) : user(pNama, pEmail) {
+        cout << "admin dibuat\n";
+    }
+    ~admin() {
+        cout << "admin dihapus\n";
+    }
+
+    void tambahMember(member* pMember) {
+        daftar_member.push_back(pMember);
+    }
+
+    void showAllMember() {
+        cout << "--- Daftar Semua Member dari Admin " << nama << " ---\n";
+        for (int i = 0; i < daftar_member.size(); i++) {
+            cout << "- " << daftar_member[i]->nama << " (Status: " << daftar_member[i]->status << ")\n";
+        }
+        cout << "\n";
+    }
+
+    void toggleActivationMember(member* pMember) {
+        if (pMember->status == "Aktif") {
+            pMember->status = "Nonaktif";
+        } else {
+            pMember->status = "Aktif";
+        }
+        cout << "Status dari " << pMember->nama << " telah diubah menjadi " << pMember->status << "\n\n";
+    }
+};
+#endif
