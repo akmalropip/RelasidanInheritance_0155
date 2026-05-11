8e7121a786c92f4b6a391cd52adbd5498f28bf9b
akmalropip <akmalropip@gmail.com>
2026-05-11 17:42:09 +0700

    menmbahkan class user

--- user.h
+++ user.h
@@ -4,3 +4,24 @@
 #include <string>
 using namespace std;
 
+class user {
+public:
+    static int globalId;
+    int id;
+    string nama;
+    string email;
+
+    user(string pNama, string pEmail) : nama(pNama), email(pEmail) {
+        id = generateId();
+        cout << "user dibuat\n";
+    }
+    ~user() {
+        cout << "user dihapus\n";
+    }
+
+    int generateId() {
+        globalId = globalId + 1;
+        return globalId;
+    }
+};
+#endif
